4a0e0b913de9e021b5ffc1fb302fa8f3d0481b7b
solankibhowmick <58584703+solankibhowmick@users.noreply.github.com>
2020-04-02 02:28:21 +0530

    Update ML4_python.py

--- ML4_python.py
+++ ML4_python.py
@@ -1,30 +1,32 @@
 # set player to 1
-player=1
+player = 1
 
 # set the initial state
-state=21 #number of objects
-print "The number of objects is now ",state
+state=21  # number of objects
+print("The number of objects is now ", state)
 while True:
-      # get valid move
-      move=input("What is your move? ")
-      if move in [1,2,3] and move<state:
+    # get valid move
+    print("Player",player)
+    while True:
+        move=input("What is your move? ")
+        if move in [1, 2, 3] and move<state:
             break
-      print "Illegal move."
+        print("Illegal move.")
 
-      # update the state
-      state=state - move
+    # update the state
+    state=state-move
 
-      # show the state
-      print "The number of objects is now ",state
+    # show the state
+    print("The number of objects is now ", state)
 
-      # check win status - win lose, stalemates
-      if state==1
-            print "Player ",player,"wins!"
-            break
-            
-      # switch players 2->1, 1->2 go back to the valid move line
-      if player==1;
-            player=2
-      else
-            player=1;
-print "Game is Over."
+    # check win status - win lose, stalemates
+    if (state==1):
+        print("Player ", player, "wins!")
+        break
+
+    # switch players 2->1, 1->2 go back to the valid move line
+    if (player == 1):
+        player = 2
+    else:
+        player = 1;
+print("Game is Over.")
